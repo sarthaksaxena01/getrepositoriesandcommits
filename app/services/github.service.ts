9cad5d410547a8a978f9f98c26b7c5ec63fc34a2
Sarthak Saxena <sarthaksaxena.ss1992@gmail.com>
2017-02-02 08:00:50 +0530

    Service changed for login of a user

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -4,13 +4,13 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService{
-    private username:string;
+    
     private client_id = '0fafb232217d38ee7475';
     private client_secret = 'c4c55446d2d3c4c685d372095acbabe3c10fa500';
     
     constructor(private _http: Http){
         console.log('Github Service Ready...');
-        this.username = 'bradtraversy';
+       
     }
     
     getUser(str:string){
